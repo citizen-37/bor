8e6c6690787ce518e7132599ba5dc18d4e1120a9
citizen-37 <strelkovskii.thailand@gmail.com>
2023-07-09 11:54:23 +0700

    callLog errs

--- eth/tracers/native/call.go
+++ eth/tracers/native/call.go
@@ -113,10 +113,6 @@ func (t *callTracer) CaptureEnd(output []byte, gasUsed uint64, _ time.Duration, 
 
 // CaptureState implements the EVMLogger interface to trace a single step of VM execution.
 func (t *callTracer) CaptureState(pc uint64, op vm.OpCode, gas, cost uint64, scope *vm.ScopeContext, rData []byte, depth int, err error) {
-	if err != nil {
-		return
-	}
-
 	if !t.config.WithLog {
 		return
 	}
